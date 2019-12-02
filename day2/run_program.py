ad0ee526f60680be46209e646ffe7e42d67815ca
Alessandro Dolci <alessandro.rn@gmail.com>
2019-12-02 17:13:09 +0100

    feat: solved day 2 part 2

--- day2/run_program.py
+++ day2/run_program.py
@@ -1,6 +1,10 @@
+import sys
+
 OP_SUM = 1
 OP_MUL = 2
 OP_TERM = 99
+
+OUTPUT_FIRST_CHAR = 19690720
 
 def execute_operation(op, first_term, second_term):
     if op == OP_SUM:
@@ -8,23 +12,45 @@ def execute_operation(op, first_term, second_term):
     elif op == OP_MUL:
         return first_term * second_term
 
+def run_program(line):
+    codes = line[:]
+
+    i = 0
+    current_op = int(codes[i])
+    while current_op != OP_TERM:
+        first_term_index = int(codes[i+1])
+        second_term_index = int(codes[i+2])
+        result_index = int(codes[i+3])
+
+        codes[result_index] = execute_operation(current_op, int(codes[first_term_index]), int(codes[second_term_index]))
+        
+        i += 4
+        current_op = int(codes[i])
+    
+    return codes
+
 with open("input/input-1.txt", "r") as input_file:
     for line in input_file:
-        codes = line.split(",")
-        codes[1] = 12
-        codes[2] = 2
+        processed_line = line.split(",")
+        processed_line[1] = 12
+        processed_line[2] = 2
+        
+        result_line = run_program(processed_line)
 
-        i = 0
-        current_op = int(codes[i])
-        while current_op != OP_TERM:
-            first_term_index = int(codes[i+1])
-            second_term_index = int(codes[i+2])
-            result_index = int(codes[i+3])
+        print("First run completed, result codes string: ")
+        print(result_line)
+        
+        for noun in range(0, 100):
+            for verb in range(0, 100):
+                processed_line[1] = noun
+                processed_line[2] = verb
 
-            codes[result_index] = execute_operation(current_op, int(codes[first_term_index]), int(codes[second_term_index]))
-            
-            i += 4
-            current_op = int(codes[i])
+                result_line = run_program(processed_line)
 
-        print("Execution completed, result codes string: ")
-        print(codes)
+                if result_line[0] == OUTPUT_FIRST_CHAR:
+                    print("Noun: " + str(noun) + ", verb: " + str(verb) + ", run completed, result code strings: ")
+                    print(result_line)
+                    print("Expected result is: " + str((100 * noun) + verb))
+
+                    sys.exit(0)
+        
